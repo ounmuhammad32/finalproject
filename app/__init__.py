2c76c1efd014829f02a0a0a92050289d957e46c8
ounmuhammad32 <45170041+ounmuhammad32@users.noreply.github.com>
2022-05-11 05:36:31 +0500

    test

--- app/__init__.py
+++ app/__init__.py
@@ -63,3 +63,9 @@ def create_app():
     return app
 
 
+@login_manager.user_loader
+def user_loader(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None
